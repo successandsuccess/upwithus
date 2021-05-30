5095bb81c0461e391b3dfb380ca41b80c9b40e0e
CodeStar <javelinmcdestro@gmail.com>
2021-05-30 08:55:26 +0200

    ffff

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\HomeController;
 */
 
 // Route::get('/', function () {
-//     return view('welcome');
+
 // });
 
 Route::get('/', 'Controller@index')->name('pages.home');
